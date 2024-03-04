38488e9ed5816f73be0930d9c2cc72d841b2400d
pestopasta74 <prestonwhiteman07@gmail.com>
2024-03-04 11:34:00 +0000

    just got told to do algorithm things

--- Coding in lesson/fib.py
+++ Coding in lesson/fib.py
@@ -15,3 +15,18 @@ def count(n):
     return
 
 print(count(5))
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)) # 4 time complexity is O(log n)
